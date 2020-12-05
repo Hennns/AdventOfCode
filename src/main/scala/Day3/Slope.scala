bf45ff0af569ba68f3fec8605462048532d29940
Hennns <henningkjoita@gmail.com>
2020-12-05 14:31:19 +0100

    add scalafmt

--- src/main/scala/Day3/Slope.scala
+++ src/main/scala/Day3/Slope.scala
@@ -6,25 +6,32 @@ import Input._
 
 case class Slope(stepsRight: Int, stepsDown: Int = 1) {
   private val maxInputLengthBeforeRepeating: Int = firstLineOfPuzzleInput.length
-  private val puzzleArray: Array[Array[Char]] = puzzleInput.toCharArray.grouped(maxInputLengthBeforeRepeating).toArray
-
+  private val puzzleArray: Array[Array[Char]] =
+    puzzleInput.toCharArray.grouped(maxInputLengthBeforeRepeating).toArray
 
   def countTrees(): Long = {
     @tailrec
-    def skipLines(i: Int, lst: List[Array[Char]], acc: List[Array[Char]]): List[Array[Char]] = (i, lst) match {
-      case (_, Nil) => acc
-      case (1, _ :: xs) => skipLines(stepsDown, xs, acc)
-      case (i, x :: xs) => skipLines(i - 1, xs, x :: acc)
-    }
+    def skipLines(
+        i: Int,
+        lst: List[Array[Char]],
+        acc: List[Array[Char]]
+    ): List[Array[Char]] =
+      (i, lst) match {
+        case (_, Nil)     => acc
+        case (1, _ :: xs) => skipLines(stepsDown, xs, acc)
+        case (i, x :: xs) => skipLines(i - 1, xs, x :: acc)
+      }
 
-    val filteredLines = if (stepsDown > 1 ) skipLines(stepsDown, puzzleArray.toList, Nil).reverse else puzzleArray.toList
+    val filteredLines =
+      if (stepsDown > 1) skipLines(stepsDown, puzzleArray.toList, Nil).reverse
+      else puzzleArray.toList
 
-    val(count, _) = filteredLines.foldLeft(0L, 0){
-      case ((count, index), line ) =>{
+    val (count, _) = filteredLines.foldLeft(0L, 0) {
+      case ((count, index), line) => {
         val newIndex = if (line.length <= index) index - line.length else index
         line(newIndex) match {
           case s: Char if (s == '#') => (count + 1, newIndex + stepsRight)
-          case _ => (count, newIndex + stepsRight)
+          case _                     => (count, newIndex + stepsRight)
         }
       }
     }
@@ -32,8 +39,7 @@ case class Slope(stepsRight: Int, stepsDown: Int = 1) {
   }
 }
 
-
-object Slope{
+object Slope {
   val slope1 = Slope(1)
   val slope2 = Slope(3)
   val slope3 = Slope(5)
@@ -41,5 +47,7 @@ object Slope{
   val slope5 = Slope(1, 2)
 
   val answerPart1: Long = slope2.countTrees()
-  val answerPart2: BigInt = slope1.countTrees() * slope2.countTrees() * slope3.countTrees() * slope4.countTrees() * slope5.countTrees()
+  val answerPart2: BigInt =
+    slope1.countTrees() * slope2.countTrees() * slope3.countTrees() * slope4
+      .countTrees() * slope5.countTrees()
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,9 @@
-
-
-
-object Main extends App{
+object Main extends App {
   println("Solution for day 3 part 1 is")
   println(Day3.Slope.answerPart1)
 
   println("")
   println("Solution for day 3 part 2 is")
   println(Day3.Slope.answerPart2)
-
-
-
-
-
-
-
-
-
 
 }
